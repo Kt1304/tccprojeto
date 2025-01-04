89b7aca022016b353e9601faac22bc053e20ebb7
Ancini <131417388+Ancini@users.noreply.github.com>
2025-01-03 23:00:58 -0300

    Update login_screen.dart

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:login_ui/Cadastro.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 import 'package:email_validator/email_validator.dart';
 import 'home_screen.dart';
+import 'package:login_ui/RecuperarSenha.dart';
 
 class MyApp extends StatelessWidget {
   @override
@@ -31,60 +32,53 @@ class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  bool isLoading = false;
+  bool _isLoading = false;
+  bool _obscurePassword = true;
 
   Future<void> _login() async {
     setState(() {
-      isLoading = true;
+      _isLoading = true;
     });
 
     final email = _emailController.text.trim();
     final password = _passwordController.text.trim();
 
-    final QueryBuilder<ParseObject> queryUsuario =
-        QueryBuilder<ParseObject>(ParseObject('Usuario'))
-          ..whereEqualTo('Email', email)
-          ..whereEqualTo('Senha', password);
-
-    final ParseResponse response = await queryUsuario.query();
+    // Usando ParseUser para realizar login
+    final ParseUser user = ParseUser(email, password, null);
+    final ParseResponse response = await user.login();
 
     setState(() {
-      isLoading = false;
+      _isLoading = false;
     });
 
-    if (response.success &&
-        response.results != null &&
-        response.results!.isNotEmpty) {
-      final ParseObject user = response.results!.first as ParseObject;
-      final String objectId = user.objectId!;
+    if (response.success && response.result != null) {
+      final ParseUser loggedInUser = response.result;
+      final String objectId = loggedInUser.objectId!;
 
-     ScaffoldMessenger.of(context).showSnackBar(
-  const SnackBar(content: Text('Login realizado com sucesso!')),
-);
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Login realizado com sucesso!')),
+      );
 
-    // Navegação para HomeScreen, passando apenas o objectId
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(
-        builder: (context) => HomeScreen(objectId: objectId),
-      ),
-    );
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => HomeScreen(objectId: objectId),
+        ),
+      );
     } else {
-      final QueryBuilder<ParseObject> queryEmail =
-          QueryBuilder<ParseObject>(ParseObject('Usuario'))
-            ..whereEqualTo('Email', email);
-      final ParseResponse emailResponse = await queryEmail.query();
-
-      if (emailResponse.success &&
-          emailResponse.results != null &&
-          emailResponse.results!.isNotEmpty) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Senha incorreta')),
-        );
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Email não encontrado')),
-        );
+      if (response.error != null) {
+        if (response.error?.code == 101) {
+          // Senha incorreta
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Senha incorreta')),
+          );
+        } else {
+          // Outros erros
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+                content: Text(response.error?.message ?? 'Erro desconhecido')),
+          );
+        }
       }
     }
   }
@@ -97,97 +91,139 @@ class _LoginPageState extends State<LoginPage> {
         child: SingleChildScrollView(
           child: Container(
             padding: const EdgeInsets.all(20),
-            constraints: const BoxConstraints(
-                maxWidth: 400), // Tamanho fixo do formulário
+            constraints: const BoxConstraints(maxWidth: 400),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                const Text(
-                  'Entrar',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 40,
-                  ),
-                ),
+                Image.asset('assets/imag/Sigga.png',
+                    width: 350, height: 350, fit: BoxFit.contain),
                 const SizedBox(height: 40),
                 Form(
                   key: _formKey,
                   child: Column(
                     children: [
-                      TextFormField(
-                        controller: _emailController,
-                        validator: (value) => EmailValidator.validate(value!)
-                            ? null
-                            : "Por favor, digite um email válido",
-                        maxLines: 1,
-                        decoration: InputDecoration(
-                          hintText: 'Email',
-                          prefixIcon: const Icon(Icons.email),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      TextFormField(
-                        controller: _passwordController,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Por favor, digite uma senha válida';
-                          }
-                          return null;
-                        },
-                        maxLines: 1,
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          hintText: 'Senha',
-                          prefixIcon: const Icon(Icons.lock),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 30),
-                      ElevatedButton(
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            _login();
-                          }
-                        },
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: const Size(
-                              double.infinity, 50), // Botão largura cheia
-                        ),
-                        child: isLoading
-                            ? const CircularProgressIndicator()
-                            : const Text(
-                                'Entrar',
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                      ),
+                      _buildEmailField(),
                       const SizedBox(height: 15),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const Cadastro()),
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: const Size(double.infinity, 50),
-                        ),
-                        child: const Text(
-                          'Cadastre-se',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ),
+                      _buildPasswordField(),
+                      const SizedBox(height: 15),
+                      _buildLoginButton(),
+                      const SizedBox(height: 15),
+                      _buildRegisterButton(),
+                      const SizedBox(height: 15),
+                      _buildForgotPasswordButton(),
+                      const SizedBox(height: 15),
                     ],
                   ),
-                )
+                ),
               ],
             ),
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEmailField() {
+    return TextFormField(
+      controller: _emailController,
+      validator: (value) => EmailValidator.validate(value!)
+          ? null
+          : "Por favor, digite um email válido",
+      maxLines: 1,
+      decoration: InputDecoration(
+        hintText: 'Email',
+        prefixIcon: const Icon(Icons.email),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPasswordField() {
+    return TextFormField(
+      controller: _passwordController,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Por favor, digite uma senha válida';
+        }
+        return null;
+      },
+      maxLines: 1,
+      obscureText: _obscurePassword,
+      decoration: InputDecoration(
+        hintText: 'Senha',
+        prefixIcon: const Icon(Icons.lock),
+        suffixIcon: IconButton(
+          icon: Icon(
+            _obscurePassword ? Icons.visibility : Icons.visibility_off,
+          ),
+          onPressed: () {
+            setState(() {
+              _obscurePassword = !_obscurePassword;
+            });
+          },
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoginButton() {
+    return ElevatedButton(
+      onPressed: _isLoading
+          ? null
+          : () {
+              if (_formKey.currentState!.validate()) {
+                _login();
+              }
+            },
+      style: ElevatedButton.styleFrom(
+        minimumSize: const Size(double.infinity, 50),
+      ),
+      child: _isLoading
+          ? const CircularProgressIndicator()
+          : const Text(
+              'Entrar',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+    );
+  }
+
+  Widget _buildRegisterButton() {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const Cadastro()),
+        );
+      },
+      style: ElevatedButton.styleFrom(
+        minimumSize: const Size(double.infinity, 50),
+      ),
+      child: const Text(
+        'Cadastre-se',
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  Widget _buildForgotPasswordButton() {
+    return TextButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const RecuperarSenha(), // Tela de recuperação
+          ),
+        );
+      },
+      child: const Text(
+        'Esqueceu a senha?',
+        style: TextStyle(
+          color: Colors.white,
         ),
       ),
     );
